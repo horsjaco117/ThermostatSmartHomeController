3e453474e465571e9ede4dec2932163ad1386bb0
horsjaco117 <jacob_horsley@live.com>
2025-12-06 16:03:47 -0700

    Add project files.

---
+++ ThermostatSmartHomeController/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ReadTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SetButton = New System.Windows.Forms.Button()
+        Me.SetTempTextBox = New System.Windows.Forms.TextBox()
+        Me.SetTempLabel = New System.Windows.Forms.Label()
+        Me.CurrentTempLabel = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.CurrentTempTextBox = New System.Windows.Forms.TextBox()
+        Me.SerialTextBox = New System.Windows.Forms.TextBox()
+        Me.SerialDataLabel = New System.Windows.Forms.Label()
+        Me.COMButton = New System.Windows.Forms.Button()
+        Me.PortsComboBox = New System.Windows.Forms.ComboBox()
+        Me.PortsComboBoxLabel = New System.Windows.Forms.Label()
+        Me.ConnectionStatusLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(641, 362)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(124, 76)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'SetButton
+        '
+        Me.SetButton.Location = New System.Drawing.Point(492, 376)
+        Me.SetButton.Name = "SetButton"
+        Me.SetButton.Size = New System.Drawing.Size(118, 62)
+        Me.SetButton.TabIndex = 1
+        Me.SetButton.Text = "Set"
+        Me.SetButton.UseVisualStyleBackColor = True
+        '
+        'SetTempTextBox
+        '
+        Me.SetTempTextBox.Location = New System.Drawing.Point(57, 98)
+        Me.SetTempTextBox.Name = "SetTempTextBox"
+        Me.SetTempTextBox.Size = New System.Drawing.Size(100, 26)
+        Me.SetTempTextBox.TabIndex = 2
+        '
+        'SetTempLabel
+        '
+        Me.SetTempLabel.AutoSize = True
+        Me.SetTempLabel.Location = New System.Drawing.Point(53, 78)
+        Me.SetTempLabel.Name = "SetTempLabel"
+        Me.SetTempLabel.Size = New System.Drawing.Size(188, 30)
+        Me.SetTempLabel.TabIndex = 3
+        Me.SetTempLabel.Text = "Set temperature"
+        '
+        'CurrentTempLabel
+        '
+        Me.CurrentTempLabel.AutoSize = True
+        Me.CurrentTempLabel.Location = New System.Drawing.Point(359, 148)
+        Me.CurrentTempLabel.Name = "CurrentTempLabel"
+        Me.CurrentTempLabel.Size = New System.Drawing.Size(236, 30)
+        Me.CurrentTempLabel.TabIndex = 4
+        Me.CurrentTempLabel.Text = "Current Temperature"
+        '
+        'CurrentTempTextBox
+        '
+        Me.CurrentTempTextBox.Location = New System.Drawing.Point(363, 171)
+        Me.CurrentTempTextBox.Name = "CurrentTempTextBox"
+        Me.CurrentTempTextBox.Size = New System.Drawing.Size(100, 26)
+        Me.CurrentTempTextBox.TabIndex = 5
+        '
+        'SerialTextBox
+        '
+        Me.SerialTextBox.Location = New System.Drawing.Point(57, 171)
+        Me.SerialTextBox.Name = "SerialTextBox"
+        Me.SerialTextBox.Size = New System.Drawing.Size(260, 26)
+        Me.SerialTextBox.TabIndex = 6
+        '
+        'SerialDataLabel
+        '
+        Me.SerialDataLabel.AutoSize = True
+        Me.SerialDataLabel.Location = New System.Drawing.Point(63, 148)
+        Me.SerialDataLabel.Name = "SerialDataLabel"
+        Me.SerialDataLabel.Size = New System.Drawing.Size(132, 30)
+        Me.SerialDataLabel.TabIndex = 7
+        Me.SerialDataLabel.Text = "Serial Data"
+        '
+        'COMButton
+        '
+        Me.COMButton.Location = New System.Drawing.Point(285, 336)
+        Me.COMButton.Name = "COMButton"
+        Me.COMButton.Size = New System.Drawing.Size(178, 92)
+        Me.COMButton.TabIndex = 9
+        Me.COMButton.Text = "COM"
+        Me.COMButton.UseVisualStyleBackColor = True
+        '
+        'PortsComboBox
+        '
+        Me.PortsComboBox.FormattingEnabled = True
+        Me.PortsComboBox.Location = New System.Drawing.Point(50, 336)
+        Me.PortsComboBox.Name = "PortsComboBox"
+        Me.PortsComboBox.Size = New System.Drawing.Size(121, 28)
+        Me.PortsComboBox.TabIndex = 10
+        '
+        'PortsComboBoxLabel
+        '
+        Me.PortsComboBoxLabel.AutoSize = True
+        Me.PortsComboBoxLabel.Location = New System.Drawing.Point(53, 303)
+        Me.PortsComboBoxLabel.Name = "PortsComboBoxLabel"
+        Me.PortsComboBoxLabel.Size = New System.Drawing.Size(113, 20)
+        Me.PortsComboBoxLabel.TabIndex = 11
+        Me.PortsComboBoxLabel.Text = "Available Ports"
+        '
+        'ConnectionStatusLabel
+        '
+        Me.ConnectionStatusLabel.AutoSize = True
+        Me.ConnectionStatusLabel.Location = New System.Drawing.Point(12, 418)
+        Me.ConnectionStatusLabel.Name = "ConnectionStatusLabel"
+        Me.ConnectionStatusLabel.Size = New System.Drawing.Size(212, 30)
+        Me.ConnectionStatusLabel.TabIndex = 12
+        Me.ConnectionStatusLabel.Text = "Connection Status"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(370, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ConnectionStatusLabel)
+        Me.Controls.Add(Me.PortsComboBoxLabel)
+        Me.Controls.Add(Me.PortsComboBox)
+        Me.Controls.Add(Me.COMButton)
+        Me.Controls.Add(Me.SerialDataLabel)
+        Me.Controls.Add(Me.SerialTextBox)
+        Me.Controls.Add(Me.CurrentTempTextBox)
+        Me.Controls.Add(Me.CurrentTempLabel)
+        Me.Controls.Add(Me.SetTempLabel)
+        Me.Controls.Add(Me.SetTempTextBox)
+        Me.Controls.Add(Me.SetButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ReadTimer As Timer
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents SetButton As Button
+    Friend WithEvents SetTempTextBox As TextBox
+    Friend WithEvents SetTempLabel As Label
+    Friend WithEvents CurrentTempLabel As Label
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents CurrentTempTextBox As TextBox
+    Friend WithEvents SerialTextBox As TextBox
+    Friend WithEvents SerialDataLabel As Label
+    Friend WithEvents COMButton As Button
+    Friend WithEvents PortsComboBox As ComboBox
+    Friend WithEvents PortsComboBoxLabel As Label
+    Friend WithEvents ConnectionStatusLabel As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ ThermostatSmartHomeController/Form1.vb
@@ -0,0 +1,219 @@
+﻿
+
+' Jacob Horsley
+' RCET 0265
+' Spring 2025
+' URL: https://github.com/horsjaco117/ThermostatControl 
+' Modified from GraphicsExamples to control a thermostat via serial communication
+Option Strict On
+Option Explicit On
+
+Imports System.IO.Ports
+Imports System.Media
+Imports System.Threading.Thread
+Imports System.Runtime.CompilerServices
+' Serial communications imports
+
+Imports System.Net.Configuration
+Public Class Form1
+
+    ' Serial Communications----------------------------------------------------------
+    Sub SetDefaults() ' Set's default serial pieces and shows COM ports
+        SerialPort1.Close() ' Closes the COM ports but adds settings
+        ConnectionStatusLabel.Text = "No connection" ' No connect until port chosen
+        GetPorts() ' Shows available ports
+    End Sub
+
+    Sub GetPorts()
+        Dim ports() = SerialPort.GetPortNames() ' Available ports (note: fixed to SerialPort.GetPortNames)
+
+        PortsComboBox.Items.Clear() ' Clears the ports
+
+        For Each port In ports ' For every port available add it to the combobox
+            PortsComboBox.Items.Add(port)
+        Next
+
+        Try
+            PortsComboBox.SelectedIndex = 0
+        Catch ex As Exception
+            ' No results
+        End Try
+    End Sub
+
+    Sub AutoConnect()
+        GetPorts() ' Get available ports
+        For Each port As String In PortsComboBox.Items
+            PortsComboBox.SelectedItem = port
+            Try
+                connect()
+                write() ' Send handshake
+                Sleep(200) ' Brief delay to allow response
+                If SerialPort1.IsOpen AndAlso IsQuietBoard() Then
+                    ConnectionStatusLabel.Text = $"Connected automatically on {SerialPort1.PortName}"
+                    Exit For ' Successful connection, stop trying other ports
+                Else
+                    SerialPort1.Close()
+                End If
+            Catch ex As Exception
+                ' Skip invalid port
+            End Try
+        Next
+        If Not SerialPort1.IsOpen Then
+            ConnectionStatusLabel.Text = "Auto-connect failed. Try manual connect."
+        End If
+    End Sub
+
+    Sub connect()
+        SerialPort1.Close() ' Closes the ports 
+        Try  ' All these are serial port settings. 
+            SerialPort1.BaudRate = 115200
+            SerialPort1.Parity = Parity.None
+            SerialPort1.StopBits = StopBits.One
+            SerialPort1.DataBits = 8
+            SerialPort1.PortName = CStr(PortsComboBox.SelectedItem)
+            SerialPort1.Open()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            SetDefaults()
+        End Try
+    End Sub
+
+    Sub send(data() As Byte)
+        SerialPort1.ReadExisting()
+        SerialPort1.Write(data, 0, UBound(data) + 1) ' Fixed to include full array length
+    End Sub
+
+    Function recieve() As Byte() ' For every byte received it will be displayed as the data variable
+        Dim data(SerialPort1.BytesToRead - 1) As Byte ' Fixed indexing
+        SerialPort1.Read(data, 0, SerialPort1.BytesToRead)
+        Return data
+    End Function
+
+    ' Write handshake
+    Sub write() ' This write sub is especially for handshaking
+        Dim data(0) As Byte
+        data(0) = &B11110000
+        SerialPort1.Write(data, 0, 1)
+    End Sub
+
+    Private Sub COMButton_Click(sender As Object, e As EventArgs) Handles COMButton.Click
+        ' Establishes the com between the device (manual connect fallback)
+        connect()
+        write()
+    End Sub
+
+    Private Sub ReadTimer_Tick(sender As Object, e As EventArgs) Handles ReadTimer.Tick
+        ReadTemperature() ' For every timer tick, request temperature data
+    End Sub
+
+    Sub ReadTemperature() ' Requests temperature data (repurposed from AnalogRead3)
+        ' Clear the previous data output for a clean read
+        If SerialTextBox IsNot Nothing Then
+            SerialTextBox.Text = String.Empty
+        End If
+
+        ' Clear the temperature display
+        If CurrentTempTextBox IsNot Nothing Then
+            CurrentTempTextBox.Text = String.Empty
+        End If
+
+        Dim data(0) As Byte
+        data(0) = &H53 ' Command to request temperature data (assuming analog channel for temp sensor)
+        If SerialPort1.IsOpen Then
+            SerialPort1.Write(data, 0, 1)
+        End If
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Try
+            Dim bytesToRead As Integer = SerialPort1.BytesToRead
+            If bytesToRead < 4 Then Exit Sub ' Not enough data yet (assuming 4-byte response as in original)
+
+            Dim buffer(bytesToRead - 1) As Byte
+            SerialPort1.Read(buffer, 0, bytesToRead)
+
+            Dim hexString As New System.Text.StringBuilder()
+            For Each b As Byte In buffer
+                hexString.Append(b.ToString("X2") & " ")
+            Next
+
+            ' Assume buffer(1) is the low byte of temperature (0-255 mapped to 0-100°C for example)
+            Dim tempValue As Integer = buffer(1) ' Repurposed from leftValue; adjust scaling as needed
+            Dim scaledTemp As Single = CSng(tempValue) / 2.55F ' Example scaling: 0-255 -> 0-100°C
+
+            ' Update the temperature text box (thread-safe)
+            WriteToTextBox(Me.CurrentTempTextBox, scaledTemp.ToString("F1") & " °C")
+
+            Me.Invoke(Sub()
+                          SerialTextBox.AppendText(hexString.ToString() & vbCrLf)
+                      End Sub)
+
+        Catch ex As Exception
+            Console.WriteLine($"Serial read error: {ex.Message}")
+        End Try
+    End Sub
+
+    Function IsQuietBoard() As Boolean
+        Dim data(0) As Byte
+        data(0) = &B11110000
+        send(data)
+        Sleep(100) ' Wait for potential response
+        If SerialPort1.BytesToRead > 0 Then
+            Dim resp() As Byte = recieve()
+            ' Assume response byte 0 is &HAA for successful handshake (adjust based on actual device response)
+            If resp.Length > 0 AndAlso resp(0) = &HAA Then
+                Return True
+            End If
+        End If
+        Return False
+    End Function
+
+    Public Sub WriteToTextBox(ByVal targetTextBox As System.Windows.Forms.TextBox, ByVal content As String)
+        ' Check if the call is coming from a different thread than the one that created the control
+        If targetTextBox.InvokeRequired Then
+            ' If it is, use Invoke to marshal the call back to the UI thread
+            targetTextBox.Invoke(New Action(Sub()
+                                                targetTextBox.Text = content
+                                            End Sub))
+        Else
+            ' If it is the UI thread, update directly
+            targetTextBox.Text = content
+        End If
+    End Sub
+
+    ' New sub to send setpoint to the thermostat device
+    Sub SendSetpoint()
+        Try
+            Dim setpoint As Integer = CInt(SetTempTextBox.Text) ' Get user-entered setpoint
+            If setpoint < 0 Or setpoint > 100 Then ' Example validation
+                MsgBox("Setpoint must be between 0 and 100.")
+                Exit Sub
+            End If
+
+            Dim data(1) As Byte
+            data(0) = &H60 ' Invented command byte for setting temperature (adjust to actual protocol)
+            data(1) = CByte(setpoint * 2.55F) ' Scale 0-100 to 0-255 byte
+            send(data)
+            MsgBox("Setpoint sent successfully.")
+        Catch ex As Exception
+            MsgBox($"Error sending setpoint: {ex.Message}")
+        End Try
+    End Sub
+
+    ' Event Handlers-----------------------------------------
+    Private Sub SetButton_Click(sender As Object, e As EventArgs) Handles SetButton.Click
+        SendSetpoint() ' Send the setpoint when button clicked
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close() ' Closes the program upon activation from button
+    End Sub
+
+    Private Sub ThermostatControlForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults() ' Serial communication defaults
+        AutoConnect() ' Attempt automatic connection on load
+        ReadTimer.Interval = 1000 ' Set timer to poll every second (adjust as needed)
+        ReadTimer.Enabled = True ' Start constant reading
+    End Sub
+End Class
+
